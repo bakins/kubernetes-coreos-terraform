67f27970a84e326bafcbf35ee1077053baacc23f
Brian Akins <brian@akins.org>
2015-05-29 17:28:09 -0400

    general code cleanup

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -20,3 +20,29 @@ setup-install-etcd() {
   chmod +x /opt/bin/install-etcd
 }
 
+setup-install-kubernetes() {
+  mkdir -p /opt/bin
+  cp $DIR/install-kubernetes /opt/bin/install-kubernetes
+  chmod +x /opt/bin/install-kubernetes
+  cat <<EOF > /etc/systemd/system/install-kubernetes.service
+[Unit]
+Description=Install Kubernetes
+Documentation=https://github.com/GoogleCloudPlatform/kubernetes
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+ExecStart=/opt/bin/install-kubernetes
+Type=oneshot
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+setup-wupiao() {
+  mkdir -p /opt/bin
+  cp $DIR/wupiao /opt/bin/wupiao
+  chmod +x /opt/bin/wupiao
+}
