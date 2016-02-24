74ed5dda594e2304a4171ee7b60400b339cb9d65
Mettjus <karel291180@gmail.com>
2016-02-24 17:55:57 +0100

    update k8s version and implement tls
    - use coreos-config yaml to configure instances
    - create aws auto-scaling group
    - tls for workers is generated by workes themselves using the ca and ca-key uploaded via cloud-config (should probably be made more secure)

--- templates/kubectl-config.sh
+++ templates/kubectl-config.sh
@@ -1,7 +1,17 @@
-
+ 
 # Use these commands to configure kubectl
-kubectl config set-cluster ${cluster_name} --insecure-skip-tls-verify=true --server=https://${server}:6443
-kubectl config set-credentials admin --token='${token}'
+kubectl config set-cluster ${cluster_name} --server=https://${server}:${api_secure_port} --certificate-authority=${cwd}/tls-assets/ca.pem
+kubectl config set-credentials admin --certificate-authority=${cwd}/tls-assets/ca.pem --client-key=${cwd}/tls-assets/apiserver-key.pem --client-certificate=${cwd}/tls-assets/apiserver.pem
 kubectl config set-context ${cluster_name} --cluster=${cluster_name} --user=admin
 kubectl config use-context ${cluster_name}
 
+# As an alternative you can run all kubectl commands with the --kubeconfig config pointing to the 'kubecfg' file in this folder. Example:
+kubectl --kubeconfig=${cwd}/kubeconfig get pods
+
+# ... or ...
+export KUBECONFIG=${cwd}/kubeconfig
+kubectl get pods
+
+#Insecure config stuff
+# kubectl config set-cluster ${cluster_name} --insecure-skip-tls-verify=true --server=http://${server}:8080
+# kubectl config set-credentials admin --token='${token}'
--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
 variable "access_key" {}
-
 variable "secret_key" {}
 
 variable "region" {
@@ -26,12 +25,28 @@ variable "num_etcd" {
     default = 3
 }
 
+variable "num_master" {
+    default = 3
+}
+
 variable "num_worker" {
     default = 5
 }
 
 variable "ami" {
     default = "ami-23b58613"
+}
+
+variable "etcd_instance_type" {
+    default = "t2.small"
+}
+
+variable "master_instance_type" {
+    default = "t2.small"
+}
+
+variable "worker_instance_type" {
+    default = "m3.medium"
 }
 
 variable "kubernetes_version" {
@@ -41,4 +56,28 @@ variable "kubernetes_version" {
 variable "etcd_version" {
     description = "version of etcd to use"
     default = "2.0.11"
+}
+
+variable "pod_network" {
+    default = "10.2.0.0/16"
+}
+
+variable "service_ip_range" {
+    default = "10.3.0.0/24"
+}
+
+variable "k8s_service_ip" {
+    default = "10.3.0.1"
+}
+
+variable "dns_service_ip" {
+    default = "10.3.0.10"
+}
+
+variable "api_secure_port" {
+    default = "443"
+}
+
+variable "master_dns_name" {
+    default = ""
 }
